b95ec64b17ea8cc4d2e496ddd349a96950d53e95
zdy <643684107@qq.com>
2018-08-10 10:00:56 +0800

    修改一堆东西

--- src/com/tgzzzxweb/impldao/UserDaoImp.java
+++ src/com/tgzzzxweb/impldao/UserDaoImp.java
@@ -38,54 +38,84 @@ public class UserDaoImp extends MysqlConnection implements UserDao {
 	@Override
 	public List<User> showAllUser() {
 		session = HibernateUtils.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-		List<User> list;
-        list = session.createQuery("from User").list();
-        session.getTransaction().commit();
-        return list;
+		session.getTransaction().begin();
+		try{
+			List<User> list;
+        	list = session.createQuery("from User").list();
+       	    session.getTransaction().commit();
+        	return list;
+		}catch (Exception e){
+			session.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 	public User selectByName(String userid){
 	    session = HibernateUtils.getSessionFactory().getCurrentSession();
-	    session.beginTransaction();
-	    User user;
-	    user = (User)session.createQuery("from User as us where us.userid = ?").setParameter(0,userid).uniqueResult();
-	    session.getTransaction().commit();
-	    return user;
+	    session.getTransaction().begin();
+	    try{
+	        User user;
+	        user = (User)session.createQuery("from User as us where us.userid = ?").setParameter(0,userid).uniqueResult();
+	        session.getTransaction().commit();
+	        return user;
+        }catch (Exception e){
+            session.getTransaction().rollback();
+            e.printStackTrace();
+            throw e;
+        }
     }
 
 	@Override
 	public void deleteUer(String id) {
 		session = HibernateUtils.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-		User temp = new User();
-		temp.setUserid(id);
-		session.delete(temp);
-		session.getTransaction().commit();
+		session.getTransaction().begin();
+		try {
+            User temp = new User();
+            temp.setUserid(id);
+            session.delete(temp);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            session.getTransaction().rollback();
+            e.printStackTrace();
+            throw e;
+        }
 	}
 
 	@Override
 	public void updateUser(User user, String id) {
 		session = HibernateUtils.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-		User temp = (User) session.get(User.class, id);
+		session.getTransaction().begin();
+		try{
+		    User temp = (User) session.get(User.class, id);
 		// temp.setId(user.getId());
-		temp.setName(user.getName());
-		temp.setPassword(user.getPassword());
-		temp.setUserid(user.getUserid());
-		temp.setBool(user.getBool());
-		session.update(temp);
-		session.getTransaction().commit();
+		    temp.setName(user.getName());
+		    temp.setPassword(user.getPassword());
+		    temp.setUserid(user.getUserid());
+		    temp.setBool(user.getBool());
+            session.update(temp);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            session.getTransaction().rollback();
+            e.printStackTrace();
+            throw e;
+        }
 	}
 
 	@Override
 	public boolean addUser(User user) {
 		session = HibernateUtils.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
+		session.getTransaction().begin();
 		System.out.println(user);
-		session.save(user);
-		session.getTransaction().commit();
-		return false;
+		try {
+            session.save(user);
+            session.getTransaction().commit();
+            return false;
+        }catch (Exception e){
+            session.getTransaction().rollback();
+            e.printStackTrace();
+            throw e;
+        }
 	}
 
 }
